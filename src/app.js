4fd02bbcefdb3f266773bbf15d61f233fa1bf089
GabrielIGNS <gabrielgranjeia@alunos.utfpr.edu.br>
2023-09-06 17:23:06 -0300

    adicionado um end point para testar o servidor

--- src/app.js
+++ src/app.js
@@ -1,12 +1,17 @@
 import cors from 'cors';
 import dotenv from 'dotenv';
 import express, { json } from 'express';
+import httpStatus from 'http-status';
 dotenv.config();
 
 
 const app = express();
 app.use(cors());
 app.use(json());
+
+app.get("/health", (req, res) => {
+    res.status(httpStatus.OK).send("up and runnig");
+})
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
