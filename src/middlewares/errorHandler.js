2e15c3ba85b9bbaa6ee3b4b42e4d248d2832c845
GabrielIGNS <gabrielgranjeia@alunos.utfpr.edu.br>
2023-09-11 17:15:48 -0300

    correção para deploy funcionar

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -5,5 +5,5 @@ export default function errorHandler(error, req, res, next){
     if (error.type === "incompleteData") return res.status(httpStatus.UNPROCESSABLE_ENTITY).send(error.message)
     if (error.type === "conflict") return res.status(httpStatus.CONFLICT).send(error.message)
     if (error.type === "unprocessableEntity") return res.status(httpStatus.UNPROCESSABLE_ENTITY).send(error.message)
-	return res.status(httpStatus.INTERNAL_SERVER_ERROR).send("erro interno inesperado")
+	return res.status(httpStatus.INTERNAL_SERVER_ERROR).send("erro interno inesperado, tenta colocar a data de uma forma que fique genérico dia e mês tipo 10-10-2023, estou com problemas com o datastyle no bando do deploy, ele está como MM-DD-YYYY, não tive tempo de arrumar")
 }
